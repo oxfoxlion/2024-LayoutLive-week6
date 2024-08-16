649b30a12fb414f77c07c5fd98895ea31374513d
oxfoxlion <oxfoxlion@gmail.com>
2024-08-17 00:03:25 +0800

    更新首頁響應式到"大家都在看"區塊

--- main.js
+++ main.js
@@ -5,12 +5,26 @@ const swiper1 = new Swiper('.swiper1', {
     spaceBetween: 48,
     slidesPerGroup:1,
     pagination: {
-      el:".swiper-pagination-custom1",
+      el:".swiper-pagination-custom",
       type: "fraction"
     },
     navigation: {
           nextEl: ".swiper-next1",
           prevEl: ".swiper-prev1",
+        },
+   });
+
+   const swiper1a = new Swiper('.swiper1a', {
+    slidesPerView: 1,
+    spaceBetween: 48,
+    slidesPerGroup:1,
+    pagination: {
+      el:".swiper-pagination-custom1a",
+      type: "fraction"
+    },
+    navigation: {
+          nextEl: ".swiper-next1a",
+          prevEl: ".swiper-prev1a",
         },
    });
 
@@ -24,3 +38,22 @@ const swiper1 = new Swiper('.swiper1', {
           prevEl: ".swiper-prev2",
         },
    });
+
+   const swiper3 = new Swiper('.swiper3', {
+    slidesPerView: 1,
+    spaceBetween: 48,
+    slidesPerGroup:1,
+   });
+
+
+   const hoverUps = document.querySelectorAll(".hoverUp");
+
+hoverUps.forEach(hoverUp => {
+  hoverUp.addEventListener('mouseenter', () => {
+      gsap.to(hoverUp, { y:-10 });
+  });
+
+  hoverUp.addEventListener('mouseleave', () => {
+      gsap.to(hoverUp, { y:0 });
+  });
+});
