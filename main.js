d69b6c2de72f46af5b366b57ef4148131f22be6d
oxfoxlion <oxfoxlion@gmail.com>
2024-08-16 14:40:25 +0800

    首頁更新到倒數第二個區塊

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 import './assets/scss/all.scss';
 
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
     slidesPerView: 2,
     spaceBetween: 48,
     slidesPerGroup:1,
     pagination: {
-      el:".swiper-pagination-custom",
+      el:".swiper-pagination-custom1",
       type: "fraction"
     },
     navigation: {
-          nextEl: ".swiper-next",
-          prevEl: ".swiper-prev",
+          nextEl: ".swiper-next1",
+          prevEl: ".swiper-prev1",
+        },
+   });
+
+   const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 3,
+    spaceBetween: 48,
+    slidesPerGroup:1,
+    
+    navigation: {
+          nextEl: ".swiper-next2",
+          prevEl: ".swiper-prev2",
         },
    });
